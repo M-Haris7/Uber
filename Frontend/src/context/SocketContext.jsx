a5056f18723b068d6a22e1118a94599b59e7a4a1
Mohammad Haris <91845668+M-Haris7@users.noreply.github.com>
2024-12-31 13:20:00 +0530

    updating the image source dynamically based on the selected vehicle type

--- Frontend/src/context/SocketContext.jsx
+++ Frontend/src/context/SocketContext.jsx
@@ -4,7 +4,9 @@ import { io } from 'socket.io-client';
 
 export const SocketContext = createContext();
 
-const socket = io(`${import.meta.env.VITE_BASE_URL || 'http://localhost:4000'}`); // Replace with your server URL
+console.log('Backend URL:', import.meta.env.VITE_BASE_URL);
+
+const socket = io(`${import.meta.env.VITE_BASE_URL}`); // Replace with your server URL
 
 const SocketProvider = ({ children }) => {
     useEffect(() => {
